e6481edd2b6775203497ae109824037003c3ef0f
gaurav kumar <gauravkumargk9892@gmail.com>
2021-12-31 17:54:35 +0530

    added macros

--- function-concept.cpp
+++ function-concept.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 using namespace std;
 
+/** In function overloading there are functions with same name with different parameter.
+    in this which function will be called that depends on the parameters
+**/
+
+/** example of function overloading **/
 int sum(int a, int b, int c){
     int sum = a + b + c;
     return sum;
@@ -10,6 +15,10 @@ int sum(int a, int b) {
     int sum = a + b;
     return sum;
 }
+
+/** end of example of function overloading **/
+
+
 
 
 
---
+++ macros.cpp
@@ -0,0 +1,20 @@
+//macros are used to increase efficiency of program
+// to increase efficiency it simply replace the defined code using "#define name val " and replace name with value 
+
+#include <iostream>
+
+#define area(l, b) l*b
+#define b 5
+
+using namespace std;
+
+int main() {
+    int l = 7;
+    
+    int area = area(l, b); // with help of macros that area(l, b) looking like a function it will be replaced with "l*b" and as I defined b = 5 it actually replaced with "l*5"
+    
+    cout << area;
+
+    return 0;
+}
+

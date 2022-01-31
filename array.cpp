f0f2fd2741769a47083755a5ce686fde1ed79e96
gaurav kumar <gauravkumargk9892@gmail.com>
2022-02-01 02:25:48 +0530

    added 'add-matricess program'

--- array.cpp
+++ array.cpp
@@ -16,14 +16,35 @@
 //     }
 // }
 
-#include <stdio.h>
-int main()
-{
-    int arr[] = {1, 2, 3, 4, 5};
-    int *p = arr;
-    ++*p;
-    p += 2;
-    printf("%d \n", *p);
-    printf("%d", arr[1]);
+// #include <stdio.h>
+// int main()
+// {
+//     int arr[] = {1, 2, 3, 4, 5};
+//     int *p = arr;
+//     ++*p;
+//     p += 2;
+//     printf("%d \n", *p);
+//     printf("%d", arr[1]);
+//     return 0;
+// }
+
+
+#include <iostream>
+using namespace std;
+
+int fun();
+
+int fun() {
+    int b[2] = {1,2};
+    return b;
+}
+int main() {
+
+    int b[2] = fun();
+
+    cout << b[0]; 
+ 
     return 0;
 }
+
+

d305cf27789e776377e93fe127a3b3936de6a25b
gaurav kumar <gauravkumargk9892@gmail.com>
2022-04-25 00:15:41 +0530

    update

--- file-handling/file-handling.cpp
+++ file-handling/file-handling.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main () {
 
-    ifstream infile ("../add-matrices.cpp");
-    ofstream outfile ("../add-matrices.cpp");
+    string line;
 
-    outfile << "Sum";
+    ifstream infile;
+    ofstream outfile;
+    outfile.open("experiment.txt",  ios::app);
+    infile.open("../add-matrices.cpp");
+
+    while(infile) {
+        getline(infile, line);
+        outfile << line << endl;
+    }
+
+    infile.close();
+    outfile.close();
 
     return 0;
 }

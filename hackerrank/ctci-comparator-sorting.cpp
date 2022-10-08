f08f260ce0975b24dbfc35c43a423a142d696598
gaurav kumar <gauravkumargk9892@gmail.com>
2022-10-09 00:35:34 +0530

    added comparator for sorting in descending order

---
+++ hackerrank/ctci-comparator-sorting.cpp
@@ -0,0 +1,32 @@
+#include<cmath>
+#include<cstdio>
+#include<vector>
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+struct Player {
+    int score;
+    string name;
+};
+
+class Checker{
+public:
+  	// complete this method
+    static int comparator(Player a, Player b)  {
+        if(a.score > b.score) {
+            return 1; 
+        } else if(a.score < b.score) {
+            return -1;
+        } else{
+             if(a.name > b.name) {
+                 return -1;
+             } else if (a.name < b.name) {
+                 return 1;
+             } else {
+                 return 0;
+             }
+        }
+    }
+};
+
